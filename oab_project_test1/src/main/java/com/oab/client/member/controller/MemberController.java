197e3c8a857f372e6a6864b734d737137f5d8741
su hyeon kim <vkdnj4147@gmail.com>
2019-09-17 17:06:52 +0900

    회원 탈퇴 관련 , 회원가입시 탈퇴 유무 able-disable 변경

--- oab_project_test1/src/main/java/com/oab/client/member/controller/MemberController.java
+++ oab_project_test1/src/main/java/com/oab/client/member/controller/MemberController.java
@@ -2,34 +2,40 @@ package com.oab.client.member.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.oab.admin.login.controller.AdminLoginController;
 import com.oab.client.login.service.LoginService;
 import com.oab.client.login.vo.LoginVO;
 import com.oab.client.member.service.MemberService;
+import com.oab.client.member.service.UserMailSendService;
 import com.oab.client.member.vo.MemberVO;
+
+import lombok.extern.java.Log;
 
 @Controller
 @RequestMapping(value = "/member")
+@Log
 public class MemberController {
-	private Logger log = LoggerFactory.getLogger(AdminLoginController.class);
-	
 	@Autowired
 	private MemberService memberService;
 
 	@Autowired
 	private LoginService loginService;
+
+	@Autowired
+	private UserMailSendService sendService;
 
 	// 회원가입 폼
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
@@ -47,7 +53,7 @@ public class MemberController {
 	}
 
 	// 회원가입 처리
-	@RequestMapping(value = "/join", method = RequestMethod.POST,  produces = "text/plain; charset=UTF-8")
+	@RequestMapping(value = "/join", method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
 	public ModelAndView memberInsert(@ModelAttribute MemberVO mvo) {
 		log.info("join.do post 방식에 의한 메서드 호출 성공");
 		ModelAndView mav = new ModelAndView();
@@ -73,7 +79,7 @@ public class MemberController {
 		return mav;
 	}
 
-	@RequestMapping(value = "/modify.do", method = RequestMethod.GET)
+	@RequestMapping(value = "/MemberModify", method = RequestMethod.GET)
 	public ModelAndView memberModify(HttpSession session) {
 		log.info("modify.do get 방식에 의한 메서드 호출 성공");
 		ModelAndView mav = new ModelAndView();
@@ -87,11 +93,11 @@ public class MemberController {
 
 		MemberVO vo = memberService.memberSelect(login.getMt_id());
 		mav.addObject("member", vo);
-		mav.setViewName("member/modify");
+		mav.setViewName("member/MemberModify");
 		return mav;
 	}
 
-	@RequestMapping(value = "/modify.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/MemberModify", method = RequestMethod.POST)
 	public ModelAndView memberModifyProcess(@ModelAttribute("MemberVO") MemberVO mvo, HttpSession session) {
 		log.info("modify.do post 방식에 의한 메서드 호출 성공");
 		ModelAndView mav = new ModelAndView();
@@ -106,42 +112,79 @@ public class MemberController {
 		if (loginService.loginSelect(mvo.getMt_id(), mvo.getOldmt_pw()) == null) {
 			mav.addObject("errCode", 1);
 			mav.addObject("member", vo);
-			mav.setViewName("member/modify");
+			mav.setViewName("member/MemberModify");
 			return mav;
 		}
 		if (memberService.memberUpdate(mvo)) {
-			mav.setViewName("redirect:/member/logout.do");
+			mav.setViewName("redirect:/login/login");
 			return mav;
 		} else {
 			mav.addObject("errCode", 2);
 			mav.addObject("member", vo);
-			mav.setViewName("member/modify");
+			mav.setViewName("member/MemberModify");
 			return mav;
 		}
 	}
 
-	@RequestMapping("/delete.do")
-	public ModelAndView memberDelete(HttpSession session) {
-		log.info("delete.do get방식에 의한 메서드 호출 성공");
-
+	// 회원 탈퇴
+	@RequestMapping(value = "/memberSecede", method = RequestMethod.GET)
+	public ModelAndView memberSeceUpdate(HttpSession session) {
+		log.info("memberSecede.do get 방식에 의한 메서드 호출 성공");
 		ModelAndView mav = new ModelAndView();
+
 		LoginVO login = (LoginVO) session.getAttribute("login");
 
 		if (login == null) {
 			mav.setViewName("login/login");
-			return mav;
+			return mav;// 로그인이 안되어있다면 로그인하라고 로그인화면을 보여줌
 		}
 
-		int errCode = memberService.memberDelete(login.getMt_id());
-		switch (errCode) {
-		case 2:
-			mav.setViewName("redirect:/member/logout.do");
-			break;
-		case 3:
-			mav.addObject("errCode", 3);
-			mav.setViewName("login/login");
-			break;
+		MemberVO vo = memberService.memberSelect(login.getMt_id());
+		mav.addObject("member", vo);
+		mav.setViewName("member/memberSecede");
+		return mav;
+	}
+
+	
+
+	// 회원 비활성화 (강좌상태 'ABLE' -> 'DISABLE')
+	@RequestMapping(value = "/memberSecede/{mt_id}", method = { RequestMethod.PUT, RequestMethod.PATCH })
+	public ResponseEntity<String> memberSecede(@PathVariable("mt_id") String mt_id, @RequestBody MemberVO mvo) {
+		System.out.println("memberSecede 호출 성공");
+		System.out.println(mvo.getMt_id());
+
+		ResponseEntity<String> entity = null;
+		try {
+			mvo.setMt_id(mt_id);
+			memberService.memberSecede(mvo);
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
+		return entity;
+	}
+
+	// 임시 비밀번호 발급
+	@RequestMapping("/memberPwUpdate.do")
+	public ModelAndView memberPwUpdate(HttpSession session, LoginVO lvo) throws Exception {
+		log.info("memberPwUpdate.do get방식에 의한 메서드 호출 성공");
+		ModelAndView mav = new ModelAndView();
+
+		sendService.sendMailPassword(lvo);
+		mav.setViewName("login/login");
+		return mav;
+	}
+
+	// 아이디 찾기
+	@RequestMapping("/memberIdselect.do")
+	public ModelAndView memberIdselect(HttpSession session, MemberVO mvo) throws Exception {
+		log.info("memberIdselect.do get방식에 의한 메서드 호출 성공");
+		ModelAndView mav = new ModelAndView();
+
+		sendService.sendMailId(mvo);
+
+		mav.setViewName("login/login");
 		return mav;
 	}
 
---
+++ oab_project_test1/src/main/webapp/WEB-INF/views/member/memberSecede.jsp
@@ -0,0 +1,129 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>memberSecede</title>
+<script type="text/javascript"
+	src="/resources/include/js/jquery-1.12.4.min.js"></script>
+<script type="text/javascript" src="/resources/include/js/common.js"></script>
+<!-- <script type="text/javascript"
+	src="/resources/include/js/memberSecede.js"></script> -->
+<script type="text/javascript">
+	$(document).on("click", "#memberSecede", function() {
+
+		// 입력값 체크
+		if (!formCheck($('#mt_id'), $('.error:eq(0)'), "아이디를"))
+			return;
+
+		else if (!formCheck($('#mt_pw'), $('.error:eq(1)'), "비밀번호를"))
+			return;
+
+		else {
+			var check = document.deleteform.check.checked;
+			var num = 0;
+
+			if (check == true) {
+				num = 1;
+			}
+			if (num == 1) {
+				//document.deleteform.submit();
+			} else {
+				alert("회원 탈퇴 이용약관에 동의하셔야 합니다.");
+				return true;
+
+			}
+			if (confirm("정말 탈퇴하시겠습니까?")) {
+				var mt_id = $("#mt_id").val();
+
+				$.ajax({
+					url : "/member/memberSecede/" + mt_id,
+					type : "put",
+					headers : {
+						"Content-Type" : "application/json",
+						"X-HTTP-Method-Override" : "put"
+					},
+					data : JSON.stringify({
+						mt_id : mt_id
+					}),
+					dataType : "text",
+					success : function(result) {
+						console.log("result:" + result);
+						if (result == 'SUCCESS') {
+							alert("고객님의 회원 탈퇴 처리가 완료되었습니다.");
+
+							$("#delete").attr({
+								"method" : "POST",
+								"action" : "/login/logout"
+							});
+							$("#delete").submit();
+						}
+					}
+
+				});
+			}
+
+		}
+
+	});
+
+	$("#SecedeReset").click(function() {
+		location.href = "/login/login";
+	});
+</script>
+</head>
+<body>
+	<form name="deleteform" id="delete">
+		<table width="1400" height="200">
+			<tr>
+				<td width="100%" height="10%"><span style="padding-left: 160px"></span>&nbsp;
+					<br>
+					<hr></td>
+			</tr>
+			<tr>
+				<td width="100%" height="20%" align="center">
+					<p align="left">
+						<span style="padding-left: 160px"> <b> OAB 텐트 회원 탈퇴</b></span>
+					</p>
+					<h4>유의사항 확인 후 탈퇴 신청을 해주세요</h4> <br> <textarea rows="7"
+						cols="130">
+					- 회원탈퇴를 하시면 OAB TENT 로 가입된 모든 서비스를 사용할 수 없습니다.
+					- 또한 OAB TENT 아이디로 서비스를 가입/사용 하면서 축적된 정보 및 기록은 
+					    모두 삭제 되며,복구가 불가능합니다.
+					- 대여서비를 이용하고 계신 고객님은 탈퇴가 불가능하오니, 반납후 탈퇴 진행이 가능합니다.
+   </textarea> <br> <br> <input type="checkbox" name="check"
+					class="check" id="check"> 위 내용을 모두 확인 하였으며, 탈퇴시 모든 정보가 복구
+					불가능함에 동의합니다. <br>
+					<hr>
+				</td>
+			</tr>
+		</table>
+
+
+		<div class="form-group">
+			<label for="mt_id" class="col-sm-2 control-label">아 이 디 </label>
+			<div class="col-sm-4">
+				<input type="text" id="mt_id" name="mt_id" class="form-control"
+					placeholder="ID">
+			</div>
+			<p class="form-control-static error"></p>
+		</div>
+		<div class="form-group">
+			<label for="mt_pw" class="col-sm-2 control-label">비밀번호 </label>
+			<div class="col-sm-4">
+				<input type="password" id="mt_pw" name="mt_pw" class="form-control"
+					placeholder="Password">
+			</div>
+			<p class="form-control-static error"></p>
+		</div>
+		<br> <input type="button" value="탈퇴" id="memberSecede" /> <input
+			type="button" value="취소" id="SecedeReset">
+	</form>
+
+
+</body>
+</html>
